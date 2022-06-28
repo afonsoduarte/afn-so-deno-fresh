dd9c53841615b1d8c0c95733581d861d10727ad1
Afonso Martins <a@afn.so>
2022-06-29 00:27:25 +0200

    First commit

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from 'preact'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>La Afta Ltd</h1>
+      <p>Serving the world wide web since 2001</p>
+      <p>For any enquiries, email a[at]afn.so</p>
+    </div>
+  )
+}
